f93e85138ec9cedefc43fc0c1cca54ab40cff0b0
gvanca76 <gvanca.eliauri76@gmail.com>
2022-10-11 02:05:03 +0400

    homework

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
